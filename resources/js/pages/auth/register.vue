152155ec2d00815803e52e3306849d088757f5f1
Werner Barnard <wbarnard81@gmail.com>
2019-10-17 17:38:12 +0200

    Auth fixes

--- resources/js/pages/auth/register.vue
+++ resources/js/pages/auth/register.vue
@@ -1,45 +1,67 @@
 <template>
   <div class="row">
     <div class="col-lg-8 m-auto">
-      <card v-if="mustVerifyEmail" :title="$t('register')">
-        <div class="alert alert-success" role="alert">
-          {{ $t('verify_email_address') }}
-        </div>
+      <card v-if="mustVerifyEmail" :title="'register'">
+        <div class="alert alert-success" role="alert">{{ 'verify_email_address' }}</div>
       </card>
-      <card v-else :title="$t('register')">
+      <card v-else :title="'register'">
         <form @submit.prevent="register" @keydown="form.onKeydown($event)">
           <!-- Name -->
           <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('name') }}</label>
+            <label class="col-md-3 col-form-label text-md-right">{{ 'name' }}</label>
             <div class="col-md-7">
-              <input v-model="form.name" :class="{ 'is-invalid': form.errors.has('name') }" class="form-control" type="text" name="name">
+              <input
+                v-model="form.name"
+                :class="{ 'is-invalid': form.errors.has('name') }"
+                class="form-control"
+                type="text"
+                name="name"
+              />
               <has-error :form="form" field="name" />
             </div>
           </div>
 
           <!-- Email -->
           <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('email') }}</label>
+            <label class="col-md-3 col-form-label text-md-right">{{ 'email' }}</label>
             <div class="col-md-7">
-              <input v-model="form.email" :class="{ 'is-invalid': form.errors.has('email') }" class="form-control" type="email" name="email">
+              <input
+                v-model="form.email"
+                :class="{ 'is-invalid': form.errors.has('email') }"
+                class="form-control"
+                type="email"
+                name="email"
+              />
               <has-error :form="form" field="email" />
             </div>
           </div>
 
           <!-- Password -->
           <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('password') }}</label>
+            <label class="col-md-3 col-form-label text-md-right">{{ 'password' }}</label>
             <div class="col-md-7">
-              <input v-model="form.password" :class="{ 'is-invalid': form.errors.has('password') }" class="form-control" type="password" name="password">
+              <input
+                v-model="form.password"
+                :class="{ 'is-invalid': form.errors.has('password') }"
+                class="form-control"
+                type="password"
+                name="password"
+              />
               <has-error :form="form" field="password" />
             </div>
           </div>
 
           <!-- Password Confirmation -->
           <div class="form-group row">
-            <label class="col-md-3 col-form-label text-md-right">{{ $t('confirm_password') }}</label>
+            <label class="col-md-3 col-form-label text-md-right">{{ 'confirm_password' }}</label>
             <div class="col-md-7">
-              <input v-model="form.password_confirmation" :class="{ 'is-invalid': form.errors.has('password_confirmation') }" class="form-control" type="password" name="password_confirmation">
+              <input
+                v-model="form.password_confirmation"
+                :class="{ 'is-invalid': form.errors.has('password_confirmation') }"
+                class="form-control"
+                type="password"
+                name="password_confirmation"
+              />
               <has-error :form="form" field="password_confirmation" />
             </div>
           </div>
@@ -47,9 +69,7 @@
           <div class="form-group row">
             <div class="col-md-7 offset-md-3 d-flex">
               <!-- Submit Button -->
-              <v-button :loading="form.busy">
-                {{ $t('register') }}
-              </v-button>
+              <v-button :loading="form.busy">{{ 'register' }}</v-button>
 
               <!-- GitHub Register Button -->
               <login-with-github />
@@ -62,52 +82,54 @@
 </template>
 
 <script>
-import Form from 'vform'
-import LoginWithGithub from '~/components/LoginWithGithub'
+import Form from "vform";
+import LoginWithGithub from "~/components/LoginWithGithub";
 
 export default {
-  middleware: 'guest',
+  middleware: "guest",
 
   components: {
     LoginWithGithub
   },
 
-  metaInfo () {
-    return { title: this.$t('register') }
+  metaInfo() {
+    return { title: "register" };
   },
 
   data: () => ({
     form: new Form({
-      name: '',
-      email: '',
-      password: '',
-      password_confirmation: ''
+      name: "",
+      email: "",
+      password: "",
+      password_confirmation: ""
     }),
     mustVerifyEmail: false
   }),
 
   methods: {
-    async register () {
+    async register() {
       // Register the user.
-      const { data } = await this.form.post('/api/register')
+      const { data } = await this.form.post("/api/register");
 
       // Must verify email fist.
       if (data.status) {
-        this.mustVerifyEmail = true
+        this.mustVerifyEmail = true;
       } else {
         // Log in the user.
-        const { data: { token } } = await this.form.post('/api/login')
+        const {
+          data: { token }
+        } = await this.form.post("/api/login");
 
         // Save the token.
-        this.$store.dispatch('auth/saveToken', { token })
+        this.$store.dispatch("auth/saveToken", { token });
 
         // Update the user.
-        await this.$store.dispatch('auth/updateUser', { user: data })
+        await this.$store.dispatch("auth/updateUser", { user: data });
 
         // Redirect home.
-        this.$router.push({ name: 'home' })
+        this.$router.push({ name: "home" });
       }
     }
   }
-}
+};
 </script>
