5058930eb2ab6e28026e86801c293f382fe8e440
Werner Barnard <wbarnard81@gmail.com>
2019-10-22 15:45:21 +0200

    Started with the quotes UI

--- resources/js/pages/quotes/create.vue
+++ resources/js/pages/quotes/create.vue
@@ -3,8 +3,501 @@
     <div>
       <Adminpanel />
     </div>
-    <div>
-      <h1>Add a Quote</h1>
+    <div class="container-fluid bg-white">
+      <h1 class="text-center display-4">Add a Quote</h1>
+      <br />
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-4">
+          <table class="table table-bordered table-sm">
+            <tbody>
+              <tr>
+                <th scope="row">Jobcard Number</th>
+                <td>1000</td>
+              </tr>
+              <tr>
+                <th scope="row">Client Name</th>
+                <td>Frys Metal</td>
+              </tr>
+              <tr>
+                <th scope="row">VAT Registration Number</th>
+                <td>0123 4567 023</td>
+              </tr>
+              <tr>
+                <th scope="row">Company Registration Number</th>
+                <td>201906/8800560/6</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-2"></div>
+        <div class="col-4">
+          <table class="table table-bordered table-sm">
+            <thead>
+              <th>Expenses</th>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Petrol / Diesel</th>
+                <td>R1 000</td>
+              </tr>
+              <tr>
+                <th scope="row">Electricity</th>
+                <td>R2 000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <hr />
+
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+          <div class="form-group">
+            <label for="sow">Scope of Work</label>
+            <textarea class="form-control" id="sow" rows="8"></textarea>
+          </div>
+        </div>
+        <div class="col-1"></div>
+      </div>
+
+      <hr />
+
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+          <label>Materials:</label>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>1</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>2</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>3</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>4</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>5</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>6</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>7</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>8</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>9</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>10</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>11</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>12</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>13</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>14</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>15</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>16</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>17</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>18</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>19</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>20</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>21</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>22</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>23</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>24</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="col-1">
+              <strong>25</strong>
+            </div>
+            <div class="col-8">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+            <div class="col-1">
+              <input class="form-control form-control-sm" type="text" />
+            </div>
+          </div>
+          <br />
+          <div class="row">
+            <div class="col"></div>
+            <div class="col-1">Total:</div>
+            <div class="col-1">R10 000</div>
+            <div class="col-1"></div>
+          </div>
+        </div>
+        <div class="col-1"></div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div>
+          <label>Wage Expenses</label>
+        </div>
+        <div>
+          <form>
+            <div class="row">
+              <div class="col">
+                <div class="form-group">
+                  <label for="employee">Employee</label>
+                  <input type="text" class="form-control" id="employee" placeholder="John Snow" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="rateph">Rate per Hour</label>
+                  <input type="text" class="form-control" id="rateph" placeholder="R67" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="hours">Hours Needed</label>
+                  <input type="text" class="form-control" id="hours" placeholder="40" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="total_wage">Total Wage</label>
+                  <input type="text" class="form-control" id="total_wage" placeholder="R2 240" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div>
+          <label>Overtime Expenses</label>
+        </div>
+        <div>
+          <form>
+            <div class="row">
+              <div class="col">
+                <div class="form-group">
+                  <label for="employee">Employee</label>
+                  <input type="text" class="form-control" id="employee" placeholder="John Snow" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="rateph">Rate per Hour</label>
+                  <input type="text" class="form-control" id="rateph" placeholder="R67" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="hours">Overtime Hours</label>
+                  <input type="text" class="form-control" id="hours" placeholder="40" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="total_wage">Overtime Cost</label>
+                  <input type="text" class="form-control" id="total_wage" placeholder="R2 240" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="employee">Doubletime Hours</label>
+                  <input type="text" class="form-control" id="employee" placeholder="John Snow" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="rateph">Doubletime Cost</label>
+                  <input type="text" class="form-control" id="rateph" placeholder="R67" />
+                </div>
+              </div>
+              <div class="col">
+                <div class="form-group">
+                  <label for="hours">Total Overtime</label>
+                  <input type="text" class="form-control" id="hours" placeholder="40" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <hr />
+
+      <div>
+        <div>
+          <div>Totale Wage Calculation</div>
+          <div>R35 056</div>
+        </div>
+        <div>
+          <div>Tax 15%</div>
+          <div>R5 356</div>
+        </div>
+        <div>
+          <div>Totale Wages</div>
+          <div>R40 056</div>
+        </div>
+      </div>
+
+      <div>
+        <div class>
+          <div class="d-flex justify-content-end mr-5 mb-5">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
