f0f8a027395cec4dc3d560c7c53950fa70724a04
Werner Barnard <wbarnard81@gmail.com>
2019-10-29 15:54:46 +0200

    More updates to the quote system

--- resources/js/pages/quotes/create.vue
+++ resources/js/pages/quotes/create.vue
@@ -13,12 +13,17 @@
             <tbody>
               <tr>
                 <th scope="row">Jobcard Number</th>
-                <td>1000</td>
+                <td>{{ jobcard_number }}</td>
               </tr>
               <tr>
                 <th scope="row">Customer Name</th>
                 <td>
-                  <select class="theselect">
+                  <select
+                    class="theselect"
+                    v-model="customer.customer"
+                    @change="setFields($event.target.value)"
+                  >
+                    <option disabled value>Select the Customer</option>
                     <option v-for="customer in customers" :key="customer.id">{{ customer.customer }}</option>
                   </select>
                 </td>
@@ -26,13 +31,13 @@
               <tr>
                 <th scope="row">VAT Registration Number</th>
                 <td>
-                  <input type="text" class="theselect" />
+                  <input disabled class="theselect" type="text" v-model="customer.vat_number" />
                 </td>
               </tr>
               <tr>
                 <th scope="row">Company Registration Number</th>
                 <td>
-                  <input type="text" class="theselect" />
+                  <input disabled class="theselect" type="text" v-model="customer.company_number" />
                 </td>
               </tr>
             </tbody>
@@ -43,15 +48,23 @@
           <table class="table table-bordered table-sm">
             <thead>
               <th>Expenses</th>
+              <th>Amount</th>
+              <th>Total Cost</th>
             </thead>
             <tbody>
               <tr>
                 <th scope="row">Petrol / Diesel</th>
-                <td class="text-center">R {{ fuel }}</td>
+                <td class="w-25">
+                  <input type="number" class="theselect" v-model="fuelAmount" />
+                </td>
+                <td class="text-right">R {{ calcFuel(fuel) }}</td>
               </tr>
               <tr>
                 <th scope="row">Electricity</th>
-                <td class="text-center">R {{ electricity }}</td>
+                <td class="w-25">
+                  <input type="number" class="theselect" v-model="electricityAmount" />
+                </td>
+                <td class="text-right">R {{ calcElectricity(electricity) }}</td>
               </tr>
             </tbody>
           </table>
@@ -65,7 +78,7 @@
         <div class="col-10">
           <div class="form-group">
             <h4>Scope of Work</h4>
-            <textarea class="form-control" id="sow" rows="8"></textarea>
+            <textarea class="form-control" id="sow" rows="8" v-model="scopeOfWork"></textarea>
           </div>
         </div>
         <div class="col-1"></div>
@@ -145,20 +158,6 @@
                 </td>
               </tr>
             </tbody>
-            <tfoot>
-              <tr>
-                <td colspan="5" class="text-right">Sub Total:</td>
-                <td class="text-right">{{ invoice_subtotal }}</td>
-              </tr>
-              <tr>
-                <td colspan="5" class="text-right">Tax:</td>
-                <td class="text-right">{{invoice_tax}}%</td>
-              </tr>
-              <tr>
-                <td colspan="5" class="text-right">Total:</td>
-                <td class="text-right">{{invoice_total}}</td>
-              </tr>
-            </tfoot>
           </table>
         </div>
         <div class="col-1"></div>
@@ -170,7 +169,7 @@
         <div class="d-flex justify-content-around">
           <div v-for="employee in employees" :key="employee.id">
             <div class="form-check form-check-inline">
-              <input class="form-check-input" type="checkbox" />
+              <input class="form-check-input" type="checkbox" @click="addNewWageRow(employee)" />
               <label class="form-check-label">{{ employee.first_name }}</label>
             </div>
           </div>
@@ -183,43 +182,81 @@
         <div class="col-1"></div>
         <div class="col-10">
           <div>
-            <label>Wage Expenses</label>
+            <h4>Wage Expenses</h4>
           </div>
           <div>
-            <form>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="employee">Employee</label>
-                    <input type="text" class="form-control" id="employee" placeholder="John Snow" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="rateph">Normal Time Hours</label>
-                    <input type="number" class="form-control" id="rateph" placeholder="8" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="rateph">Overtime Hours</label>
-                    <input type="number" class="form-control" id="rateph" placeholder="4" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="hours">Double Time Hours</label>
-                    <input type="number" class="form-control" id="hours" placeholder="2" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="total_wage">Total</label>
-                    <input disabled class="form-control" :placeholder="calcWageLine(1,2)" />
-                  </div>
-                </div>
-              </div>
-            </form>
+            <table class="table">
+              <thead>
+                <tr class="row">
+                  <th scope="col" class="col-1">#</th>
+                  <th scope="col" class="col-6">Employee Name</th>
+                  <th scope="col" class="col-1">Normal Hours</th>
+                  <th scope="col" class="col-1">Overtime Hours</th>
+                  <th scope="col" class="col-1">Doubletime Hours</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(employee_wage, k) in employee_wages" :key="k" class="row">
+                  <td class="col-1">
+                    <strong>{{ k+1 }}</strong>
+                  </td>
+                  <td class="col-6">
+                    <input
+                      class="form-control"
+                      type="text"
+                      style="text-align:left;"
+                      v-model="employee_wage.employee_name"
+                    />
+                  </td>
+                  <td class="col-1">
+                    <input
+                      class="form-control text-right"
+                      type="number"
+                      min="0"
+                      step=".01"
+                      v-model="employee_wage.employee_ntHours"
+                      @change="calcWageLine(employee_wage)"
+                    />
+                  </td>
+                  <td class="col-1">
+                    <input
+                      class="form-control text-right"
+                      type="number"
+                      min="0"
+                      step=".01"
+                      v-model="employee_wage.employee_overtimeHours"
+                      @change="calcWageLine(employee_wage)"
+                    />
+                  </td>
+                  <td class="col-1">
+                    <input
+                      class="form-control text-right"
+                      type="number"
+                      min="0"
+                      step=".01"
+                      v-model="employee_wage.employee_dtHours"
+                      @change="calcWageLine(employee_wage)"
+                    />
+                  </td>
+                  <td class="col-1">
+                    <input
+                      readonly
+                      class="form-control text-right"
+                      type="number"
+                      min="0"
+                      step=".01"
+                      v-model="employee_wage.line_total"
+                    />
+                  </td>
+                </tr>
+
+                <tr v-show="employee_wages.length === 0">
+                  <td colspan="6">
+                    <p class="text-center alert-danger p-2">No Employees Selected.</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
         <div class="col-1"></div>
@@ -233,44 +270,40 @@
           <table class="table table-bordered table-sm">
             <tbody>
               <tr>
-                <th scope="row">Total Material</th>
-                <td class="text-right">R 1000</td>
+                <th scope="row">Total Expenses</th>
+                <td class="text-right">R {{ expenses_subtotal }}</td>
               </tr>
               <tr>
-                <th scope="row">Total Expenses</th>
-                <td class="text-right">R 500</td>
+                <th scope="row">Total Material</th>
+                <td class="text-right">R {{ invoice_subtotal }}</td>
               </tr>
               <tr>
                 <th scope="row">Total Wages</th>
-                <td class="text-right">R 1500</td>
-              </tr>
-              <tr>
-                <th scope="row">Total Provident Fund</th>
-                <td class="text-right">R 400</td>
+                <td class="text-right">R {{ wage_subtotal }}</td>
               </tr>
               <tr>
                 <th scope="row">Other Expenses</th>
                 <td>
-                  <input type="number" class="theselect" />
+                  <input type="number" class="theselect" v-model="other_subtotal" />
                 </td>
               </tr>
               <tr>
                 <th scope="row">Consumables</th>
                 <td>
-                  <input type="number" class="theselect" />
+                  <input type="number" class="theselect" v-model="consumables_subtotal" />
                 </td>
               </tr>
               <tr>
-                <th scope="row">Total Provident Fund</th>
-                <td class="text-right">R 400</td>
+                <th scope="row">Total excl</th>
+                <td class="text-right">R {{ quote_subtotal }}</td>
               </tr>
               <tr>
                 <th scope="row">Tax @ 15%</th>
-                <td class="text-right">R 510</td>
+                <td class="text-right">R {{ quote_tax }}</td>
               </tr>
               <tr>
                 <th scope="row">Quote Total</th>
-                <td class="text-right">R 3910</td>
+                <td class="text-right">R {{ quote_total }}</td>
               </tr>
             </tbody>
           </table>
@@ -305,15 +338,32 @@ export default {
     return {
       customers: [],
       employees: [],
+      jobcard_number: "1000",
       fuel: "",
       electricity: "",
+      fuelAmount: "",
+      electricityAmount: "",
+      fuel_cost: "",
+      electricity_cost: "",
+      scopeOfWork: "",
+      selectedEmployee: "Werner",
+      ntHours: 0,
+      overtimeHours: 0,
+      dtHours: 0,
       customer: {
-        name: "",
-        vatnumber: "",
-        regnumber: ""
+        customer: "",
+        vat_number: "",
+        company_number: ""
       },
       line_number: 1,
+      wage_line_number: 1,
       invoice_subtotal: 0,
+      wage_subtotal: 0,
+      expenses_subtotal: 0,
+      other_subtotal: 0,
+      consumables_subtotal: 0,
+      quote_tax: 0,
+      quote_total: 0,
       invoice_total: 0,
       invoice_tax: 15,
       invoice_products: [
@@ -324,7 +374,8 @@ export default {
           product_qty: "",
           line_total: 0
         }
-      ]
+      ],
+      employee_wages: []
     };
   },
   methods: {
@@ -378,6 +429,25 @@ export default {
         this.invoice_total = "0.00";
       }
     },
+    calcWageTotal() {
+      let subtotal, total;
+      subtotal = this.employee_wages.reduce(function(sum, employee) {
+        let lineTotal = parseFloat(employee.line_total);
+        if (!isNaN(lineTotal)) {
+          return sum + lineTotal;
+        }
+      }, 0);
+
+      this.wage_subtotal = subtotal.toFixed(2);
+
+      total = subtotal * (this.invoice_tax / 100) + subtotal;
+      total = parseFloat(total);
+      if (!isNaN(total)) {
+        this.wage_subtotal = total.toFixed(2);
+      } else {
+        this.wage_subtotal = "0.00";
+      }
+    },
     calculateLineTotal(invoice_product) {
       let total =
         parseFloat(invoice_product.product_price) *
@@ -387,20 +457,36 @@ export default {
       }
       this.calculateTotal();
     },
-    calcWageLine(a, b) {
-      let total = parseFloat(a.product_price) * parseFloat(a.product_qty);
-      if (!isNaN(total)) {
-        a.line_total = total.toFixed(2);
-      }
-      this.calculateTotal();
+    calcFuel(cost) {
+      let subtotal = cost * this.fuelAmount;
+      this.fuel_cost = subtotal;
+      this.expenses_subtotal = this.expenses_subtotal + subtotal;
+      return subtotal;
     },
-    deleteRow(index, invoice_product) {
-      let idx = this.invoice_products.indexOf(invoice_product);
-      console.log(idx, index);
-      if (idx > -1) {
-        this.invoice_products.splice(idx, 1);
+    calcElectricity(cost) {
+      let subtotal = cost * this.electricityAmount;
+      this.electricity_cost = subtotal;
+      this.expenses_subtotal = this.expenses_subtotal + subtotal;
+      return subtotal;
+    },
+    calcWageLine(employee_wage) {
+      let total =
+        parseFloat(employee_wage.employee_rateph) *
+        parseFloat(employee_wage.employee_ntHours) *
+        parseFloat(employee_wage.employee_overtimeHours) *
+        parseFloat(employee_wage.employee_dtHours);
+      if (!isNaN(total)) {
+        employee_wage.line_total = total.toFixed(2);
       }
-      this.calculateTotal();
+      this.calcWageTotal();
+    },
+    setFields(customerName) {
+      this.customer.vat_number = this.customers.find(
+        x => x.customer === customerName
+      ).vat_number;
+      this.customer.company_number = this.customers.find(
+        x => x.customer === customerName
+      ).company_number;
     },
     addNewRow() {
       this.invoice_products.push({
@@ -411,12 +497,36 @@ export default {
         line_total: 0
       });
       this.line_number++;
+    },
+    addNewWageRow(employee) {
+      this.employee_wages.push({
+        employee_no: employee.id,
+        employee_name: employee.first_name,
+        employee_rateph: employee.rateph,
+        employee_ntHours: "",
+        employee_overtimeHours: "",
+        employee_dtHours: "",
+        line_total: 0
+      });
+      this.wage_line_number++;
     }
   },
   mounted() {
     this.getCustomers();
     this.getConfigs();
     this.getEmployees();
+  },
+  computed: {
+    quote_subtotal: function() {
+      let subtotal =
+        this.wage_subtotal +
+        this.invoice_subtotal +
+        this.other_subtotal +
+        this.consumables_subtotal +
+        this.expenses_subtotal;
+      console.log(subtotal);
+      return subtotal;
+    }
   }
 };
 </script>
