7ff0ace5b1beed88fb0b9867debb78139975dd5f
Werner Barnard <wbarnard81@gmail.com>
2019-11-03 17:18:30 +0200

    UI fix

--- resources/js/pages/jobs/create.vue
+++ resources/js/pages/jobs/create.vue
@@ -55,7 +55,7 @@
           </div>
         </div>
 
-        <div class="container mt-2">
+        <div class="container mt-2 mb-5">
           <label>Boilermakers</label>
           <div class="d-flex justify-content-around">
             <div v-for="boilermaker in boilermakers" :key="boilermaker.id">
@@ -76,7 +76,7 @@
           </div>
         </div>
 
-        <button @click.prevent="addJob()" class="btn btn-primary mt-2">Submit</button>
+        <button @click.prevent="addJob()" class="btn btn-primary mt-5">Submit</button>
       </form>
     </div>
   </div>
