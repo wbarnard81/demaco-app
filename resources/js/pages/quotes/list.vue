ddf74c7925ab15c8af9d35a19d014d2e791c88da
Werner Barnard <wbarnard81@gmail.com>
2019-11-04 14:07:06 +0200

    UI updates before working on storing the data for the quotes

--- resources/js/pages/quotes/list.vue
+++ resources/js/pages/quotes/list.vue
@@ -5,20 +5,50 @@
     </div>
     <div>
       <h1>List of Quotes</h1>
+      <div>
+        <ul>
+          <li
+            v-for="quote in quotes"
+            :key="quote.id"
+          >{{ quote.id }} - {{ quote.customer }} - {{ quote.sow }} - R Total</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Adminpanel from "../../components/Adminpanel";
+import axios from "axios";
+
 export default {
-  name: "CreateQuote",
+  name: "ListQuotes",
   metaInfo() {
-    return { title: "List Quote" };
+    return { title: "List Quotes" };
   },
   middleware: "auth",
   components: {
     Adminpanel
+  },
+  data() {
+    return {
+      quotes: []
+    };
+  },
+  methods: {
+    getQuotes() {
+      axios
+        .get("/api/quotes")
+        .then(response => {
+          this.quotes = response.data;
+        })
+        .catch(error => {
+          console.log(error.response.data.message);
+        });
+    }
+  },
+  mounted() {
+    this.getQuotes();
   }
 };
 </script>
